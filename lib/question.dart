79b106bf06f4778c1321d74faeb882276c0f70b7
bramepar <bramepar@inf.upv.es>
2022-01-16 20:14:43 +0100

    Quiz finished

--- lib/question.dart
+++ lib/question.dart
@@ -2,10 +2,7 @@ class Question {
   String _questionText;
   bool _questionAnswer;
 
-  Question(String q, bool a){
-    _questionText = q;
-    _questionAnswer = a;
-  }
+  Question(this._questionText, this._questionAnswer);
 
   bool get questionAnswer => _questionAnswer;
 
